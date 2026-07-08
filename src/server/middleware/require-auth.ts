a79578d98cb3c4954db900cd177c7ca2a18725a2
Peter Bondad <bondadpetermaironne@gmail.com>
2026-07-08 14:57:24 +0800

    refactor(auth): use betterauth access control and admin for better security in middleware

--- src/server/middleware/require-auth.ts
+++ src/server/middleware/require-auth.ts
@@ -2,7 +2,7 @@ import type { Context, Next } from "hono";
 import type { Env } from "@/server/types/hono-types";
 import { auth } from "@/lib/auth";
 
-export async function requireAdmin(c: Context<Env>, next: Next) {
+export async function requireAuth(c: Context<Env>, next: Next) {
   const session = await auth.api.getSession({
     headers: c.req.raw.headers,
   });
@@ -12,10 +12,6 @@ export async function requireAdmin(c: Context<Env>, next: Next) {
     c.set("session", null);
 
     return c.json({ message: "Unauthorized" }, 401);
-  }
-
-  if (session.user.role !== "admin") {
-    return c.json({ message: "Forbidden" }, 403);
   }
 
   c.set("user", session.user);
--- src/server/middleware/require-permissions.ts
+++ src/server/middleware/require-permissions.ts
@@ -1,19 +1,22 @@
 import { auth } from "@/lib/auth";
-import { Context, Next } from "hono";
-import { Env } from "../types/hono-types";
+import type { Context, Next } from "hono";
+import type { Env } from "@/server/types/hono-types";
 
 export function requirePermission(permissions: Record<string, string[]>) {
   return async (c: Context<Env>, next: Next) => {
     const user = c.get("user");
 
+    if (!user) {
+      return c.json({ message: "Unauthorized" }, 401);
+    }
+
     const result = await auth.api.userHasPermission({
       body: {
-        userId: user!.id, // already authenticated, so user is guaranteed to exist
+        userId: user.id,
         permissions,
       },
     });
 
-    // Check the return shape for your Better Auth version.
     if (!result.success) {
       return c.json({ message: "Forbidden" }, 403);
     }
