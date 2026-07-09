2da83e6dbd93eb9cb5973bcfb940d8c65c769c80
Peter Bondad <bondadpetermaironne@gmail.com>
2026-07-09 13:17:48 +0800

    feat(hono): add folder designated for hono and change import paths

--- src/server/hono/hono-factory.ts
+++ src/server/hono/hono-factory.ts
@@ -1,4 +1,4 @@
-import { Env } from "@/server/types/hono-types";
+import { Env } from "@/server/hono/hono-types";
 import { createFactory } from "hono/factory";
 
 export const factory = createFactory<Env>();
